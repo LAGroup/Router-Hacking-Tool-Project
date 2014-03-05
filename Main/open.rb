506e47d9f7f4a1fce266091f1819cf2fbe30d401
kbachvarova <scarver@ubuntu.(none)>
2014-03-05 03:22:57 +0200

    Editting open.rb

--- Main/open.rb
+++ Main/open.rb
@@ -1,14 +1,10 @@
-cmd = Array.new
+#Monitoring Starter
 
-cmd[0] = `sudo airmon-ng`
-cmd[1] = `sudo airmon-ng start wlan0`
+`sudo airmon-ng`
+`sudo airmon-ng start wlan0`
 
 puts "Successfully opened \n\n"
 
-#cmd[2] = `sudo airodump-ng mon0`
+#`sudo airodump-ng mon0`
 
-#BIG - C8:3A:35:31:8E:20
-bssid = "C8:3A:35:31:8E:20"
-
-#linksys (mine) - 
 
